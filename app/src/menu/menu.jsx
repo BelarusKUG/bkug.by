cffe4130d57715f93859ca551cec89bc5169af08
chipolla <labushkina@gmail.com>
2016-03-28 22:14:15 +0300

    kotlin_cat

--- app/src/menu/menu.jsx
+++ app/src/menu/menu.jsx
@@ -5,20 +5,24 @@ export default class Menu extends React.Component {
     return (
       <header className="masthead">
         <div className="masthead-inner">
-          <h1>Belarus Kotlin User Group</h1>
-          <p className="lead"></p>
-
+          <h1 className="main-title">
+            <span className="main-title__belarus">Belarus </span>
+            <span className="main-title__kotlin">Kotlin </span>
+            <span className="main-title__user-group">User Group</span>
+          </h1>
+          <img src={require("./kotlin_cat.svg")} className="kotlin-cat"/>
           <div className="colophon">
             <ul className="colophon-links">
               <li>
-                <a href="https://github.com/mdo/hyde/archive/v1.0.0.zip">Download</a>
+                <a href="#">Events</a>
               </li>
               <li>
-                <a href="https://github.com/mdo/hyde">GitHub project</a>
+                <a href="#">Speakers</a>
               </li>
-              <li>Currently v1.1</li>
+              <li>
+                <a href="#">Contacts</a>
+              </li>
             </ul>
-            <p>&copy; 2013. All rights reserved.</p>
           </div>
 
         </div>
