cf16de0384cfa762cbcc2e53904b5268fd75e949
chipolla <labushkina@gmail.com>
2016-04-13 21:27:58 +0300

    add styles for header and shedule

--- app/src/home/home.jsx
+++ app/src/home/home.jsx
@@ -8,32 +8,51 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="content container">
-                <h1>Home Page</h1>
+                <h1 className="content__header content__header--h1">Home Page</h1>
 
 
-                <h2>BKUG Meetup #2</h2>
+                <h2 className="content__header content__header--h2">BKUG Meetup #2</h2>
 
-                <p>
+                <p className="content__text">
                     12 мая пройдет второй митап Belarus Kotlin User Group который будет посвящен Dependency Injection
                     и тестированию в Kotlin.
 
                 </p>
 
-                <h3>Программа:</h3>
+                <h3 className="content__header content__header--h3">Программа:</h3>
 
-                <ul>
-                    <li>19:00 - 20:00 – Антон Руткевич сделает введение в язык и расскажет об основных особенностях языка.</li>
-                    <li>20:00 - 21:00 – Руслан Ибрагимов расскажет, как использовать Котлин на бэкенеде на примере
-                        Spring приложения, а также рассмотрит стандартную библиотеку Котлин.
+                <ul className="schedule">
+                    <li className="schedule__item">
+                        <span className="schedule__item-time">19:00 - 20:00</span>
+                        <i className="schedule__item-point">&nbsp;</i>
+                        <div className="schedule__item-info">
+                            <h4  className="schedule__item-topic">Введение в язык и основные особенности языка</h4>
+                            <small className="schedule__item-speaker">Антон Руткевич</small>
+                        </div>
                     </li>
+                    <li className="schedule__item">
+                        <span className="schedule__item-time">20:00 - 20:15</span>
+                        <i className="schedule__item-point schedule__item-point--pause">&nbsp;</i>
+                        <div className="schedule__item-info">
+                            <h4 className="schedule__item-topic schedule__item-topic--pause">Котлин на бэкенеде на примере Spring</h4>
+                        </div>
+                    </li>
+                    <li className="schedule__item">
+                        <span className="schedule__item-time">20:15 - 21:00</span>
+                        <i className="schedule__item-point">&nbsp;</i>
+                        <div className="schedule__item-info">
+                            <h4  className="schedule__item-topic">Котлин на бэкенеде на примере Spring</h4>
+                            <small className="schedule__item-speaker">Руслан Ибрагимов</small>
+                        </div>
+                    </li>                    
                 </ul>
 
-                <p>Для участия во встрече необходима предварительная регистрация
+                <p className="content__text">Для участия во встрече необходима предварительная регистрация
                     (это поможет нам подготовить необходимое количество печенек :) )</p>
 
-                <p>Место проведения: ул. Октябрьская, 16А – EventSpace. Парковка и вход через ул. Октябрьскую, 10б.</p>
+                <p className="content__text">Место проведения: ул. Октябрьская, 16А – EventSpace. Парковка и вход через ул. Октябрьскую, 10б.</p>
 
-                <h2>Карта проезда</h2>
+                <h2 className="content__header content__header--h2">Карта проезда</h2>
 
                 <div id="YMapsEvent"></div>
             </div>
