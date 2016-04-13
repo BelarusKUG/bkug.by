8cbbc45eb828fe0a69b5ec68ee227169f5d83814
Ibragimov Ruslan <r.ibragimov@itransition.com>
2016-04-13 22:31:51 +0300

    Dummy data.

--- app/src/contacts/contacts.jsx
+++ app/src/contacts/contacts.jsx
@@ -5,6 +5,34 @@ export default class Contacts extends React.Component {
         return (
             <div className="content container">
                 <h1>Contacts</h1>
+
+                <h2>Наши ссылочки</h2>
+
+                <ul>
+                    <li><a>Youtube channel</a></li>
+                    <li><a>Gitter Chat</a></li>
+                </ul>
+
+                <h2>Драйверы сообщества</h2>
+
+                <ul>
+                    <li>
+                        <p>Руслан Ибрагимов</p>
+                        <p><a href="mailto:ruslan@ibragimov.by">ruslan@ibragimov.by</a></p>
+                    </li>
+                    <li>
+                        <p>Руслан Ибрагимов</p>
+                        <p><a href="mailto:ruslan@ibragimov.by">ruslan@ibragimov.by</a></p>
+                    </li>
+                    <li>
+                        <p>Руслан Ибрагимов</p>
+                        <p><a href="mailto:ruslan@ibragimov.by">ruslan@ibragimov.by</a></p>
+                    </li>
+                    <li>
+                        <p>Руслан Ибрагимов</p>
+                        <p><a href="mailto:ruslan@ibragimov.by">ruslan@ibragimov.by</a></p>
+                    </li>
+                </ul>
             </div>
         );
     }
