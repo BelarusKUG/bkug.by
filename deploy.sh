3df6dd27110f8c6f714ed450d21712b753e44eb6
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-09-04 15:34:37 +0300

    Setup deploy on bkug.by via rsync.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+set -e # exit with nonzero exit code if anything fails
 
-echo "Works!";
+# check current branch
+if [ 'master' != $TRAVIS_BRANCH ]; then
+    echo "Deploy only on master branch. Current branch: '$branch'.";
+    exit 0;
+fi
 
-exit 0;
+# run our compile script
+npm run build
+cp robots.txt ./dist/
+
+# sync with remote folder
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/ deploy@bkug.by:~/files/bkug.by
