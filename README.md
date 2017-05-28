0d7e7be8928d3d9cd1483841e4afa398a0549c2d
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-05-28 21:03:50 +0300

    Delete old js site.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Belarus Kotlin User Group [![Build Status](https://travis-ci.org/KotlinBy/bkug.by.svg?branch=master)](https://travis-ci.org/KotlinBy/bkug.by)
+# Belarus Kotlin User Group
 
-_Присылайте ваши pull-реквесты!_
+TBA
--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react-router/history/index.d.ts" />
-/// <reference path="globals/react-router/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/webpack-env/index.d.ts" />
