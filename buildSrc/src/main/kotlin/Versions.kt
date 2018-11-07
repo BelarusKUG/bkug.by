592b2bb59d5cee22a1325b15a86412b01fa1dfd9
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-11-07 12:36:26 +0300

    Define versions in buildSrc.

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,22 @@
+object Versions {
+    val coroutinesVersion = "1.0.0"
+    val jacksonVersion = "2.9.6"
+    val httpClientVersion = "4.1.3"
+    val ymlICalVersion = "0.0.5"
+    val undertowVersion = "2.0.13.Final"
+
+    val romeVersion = "1.11.0"
+    val commonmarkVersion = "0.11.0"
+
+    val logbackVersion = "1.2.3"
+    val slf4jVersion = "1.7.25"
+    val sentryVersion = "1.7.5"
+
+    val hikariVersion = "3.2.0"
+    val postgresqlVersion = "42.2.4"
+    val jooqVersion = "3.11.4"
+    val flywayVersion = "5.1.4"
+
+    val junitVersion = "5.2.0"
+    val mockkVersion = "1.8.6"
+}
